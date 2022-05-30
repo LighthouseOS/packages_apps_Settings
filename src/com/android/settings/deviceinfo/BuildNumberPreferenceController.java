mainigautam/caf-packages_apps_Settings

Settings: Append PixelExperience version on build display id
Change-Id: I831800ae0a769513b6b0c008ba492cd8334b3b65

src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -76,7 +76,14 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String pixelExperienceVersion = VersionUtils.getPixelExperienceVersion();
+        if (!pixelExperienceVersion.equals("")){
+            sb.append("\n");
+            sb.append(pixelExperienceVersion);
+        }
+        return sb.toString();
     }
 
     @Override

src/com/android/settings/deviceinfo/VersionUtils.java
@@ -1,0 +1,10 @@
+
+package com.android.settings.deviceinfo;
+
+import android.os.SystemProperties;
+
+public class VersionUtils {
+    public static String getPixelExperienceVersion(){
+        return SystemProperties.get("org.pixelexperience.version.display","");
+    }
+}

src/com/android/settings/deviceinfo/firmwareversion/SimpleBuildNumberPreferenceController.java
@@ -21,6 +21,7 @@ import android.os.Build;
 import android.text.BidiFormatter;
 
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.deviceinfo.VersionUtils;
 
 public class SimpleBuildNumberPreferenceController extends BasePreferenceController {
 
@@ -36,6 +37,13 @@ public class SimpleBuildNumberPreferenceController extends BasePreferenceControl
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String pixelExperienceVersion = VersionUtils.getPixelExperienceVersion();
+        if (!pixelExperienceVersion.equals("")){
+            sb.append("\n");
+            sb.append(pixelExperienceVersion);
+        }
+        return sb.toString();
     }
 }

LighthouseOS/packages_apps_Settings

src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -78,7 +78,14 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String LighthouseVersion = VersionUtils.getLighthouseVersion();
+        if (!LighthouseVersion.equals("")){
+            sb.append("\n");
+            sb.append(LighthouseVersion);
+        }
+        return sb.toString();
     }
 
     @Override

src/com/android/settings/deviceinfo/VersionUtils.java
@@ -1,0 +1,10 @@
+
+package com.android.settings.deviceinfo;
+
+import android.os.SystemProperties;
+
+public class VersionUtils {
+    public static String getPixelExperienceVersion(){
+        return SystemProperties.get("ro.lighthouse.version.display","");
+    }
+}

src/com/android/settings/deviceinfo/firmwareversion/SimpleBuildNumberPreferenceController.java
@@ -21,6 +21,7 @@ import android.os.Build;
 import android.text.BidiFormatter;
 
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.deviceinfo.VersionUtils;
 
 public class SimpleBuildNumberPreferenceController extends BasePreferenceController {
 
@@ -36,6 +37,13 @@ public class SimpleBuildNumberPreferenceController extends BasePreferenceControl
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String LighthouseVersion = VersionUtils.getLighthouseVersion();
+        if (!LighthouseVersion.equals("")){
+            sb.append("\n");
+            sb.append(LighthouseVersion);
+        }
+        return sb.toString();
     }
 }